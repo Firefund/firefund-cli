fc16d33e05067a71ff552ab9b991f47f957535a0
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-08-04 18:55:49 +0200

    install bower components

--- bin/assets.js
+++ bin/assets.js
@@ -6,6 +6,14 @@ var args = process.argv.slice(2)
 var copyPath = path.resolve(__dirname, "../node_modules/.bin/bower-copy")
 
 //shell.exec("echo " + args.toString())
-console.log(copyPath + " -r " + path.resolve(process.cwd(), args[0]))
 
-shell.exec(copyPath + " -r " + path.resolve(process.cwd(), args[0]));
+// install bower components define in calling bower.json
+if(shell.which("bower")) {
+  shell.exec("bower install")
+} else {
+  console.error("You need to install bower to download the asssets.\r\nnpm install -g bower")
+  process.exit(1);
+}
+
+//console.log(copyPath + " -r " + path.resolve(process.cwd(), args[0]))
+//shell.exec(copyPath + " -r " + path.resolve(process.cwd(), args[0]));
